ede15fcd10aa381466a6ab70d836fb61eded6f9a
Alissa Seixas <alissaps@gmail.com>
2021-11-20 17:54:55 -0300

    fix justify content

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,22 +3,22 @@ import "./HomePage.css"
 
 function HomePage() {
   return (
-    <div className="homepage">
-      <main className="d-flex justify-content-center align-items-center">
-        <section>
-          <div>linha vermelha</div>
-          <h1>Have <span className="red-text">gift</span> ideas for this <span className="red-text">Xmas</span></h1>
-          <p className="text">put a little of yourself in your gifts. here you will find great ideas for gifting your loved ones in a creative and different way. try to get your hands dirty and be amazed. gift it yourself!</p>
+    <>
+      <main className="homepage">
+        <section id="main-text">
+          {/* <div>linha vermelha</div> */}
+          <h1 className="pb-3">Have <span className="red-text">gift</span> ideas for this <span className="red-text">Xmas</span></h1>
+          <p className="text pb-3">put a little of yourself in your gifts. here you will find great ideas for gifting your loved ones in a creative and different way. try to get your hands dirty and be amazed. gift it yourself!</p>
           <button type="button" className="text btn btn-dark rounded-pill">click here</button>
         </section>
 
-        <section className="d-flex justify-content-center align-items-center ">
-          <img className="w-25" src={homepageImg} alt="gift-wrapped" />
+        <section id="main-img">
           <div className="black-line"></div>
+          <img className="img" src={homepageImg} alt="gift-wrapped" />
           <div>linha vermelha</div>
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
