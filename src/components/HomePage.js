e906880450afebf7ab29fab16698d08e2acd0468
Alissa Seixas <alissaps@gmail.com>
2021-11-20 18:55:07 -0300

    add link and red lines

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,34 @@
 import homepageImg from "./assets/homepage.jpeg";
-import "./HomePage.css"
+import redLineImg from "./assets/red-line.png";
+import "./HomePage.css";
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
     <>
       <main className="homepage">
         <section id="main-text">
-          {/* <div>linha vermelha</div> */}
-          <h1 className="pb-3">Have <span className="red-text">gift</span> ideas for this <span className="red-text">Xmas</span></h1>
-          <p className="text pb-3">put a little of yourself in your gifts. here you will find great ideas for gifting your loved ones in a creative and different way. try to get your hands dirty and be amazed. gift it yourself!</p>
-          <button type="button" className="text btn btn-dark rounded-pill">click here</button>
+          <img className="red-line text-position" src={redLineImg} />
+          <h1 className="pb-3">
+            Have <span className="red-text">gift</span> ideas for this{" "}
+            <span className="red-text">Xmas</span>
+          </h1>
+          <p className="text pb-3">
+            put a little of yourself in your gifts. here you will find great
+            ideas for gifting your loved ones in a creative and different way.
+            try to get your hands dirty and be amazed. gift it yourself!
+          </p>
+          <Link to="/giftslist">
+          <button type="button" className="text btn btn-dark rounded-pill">
+            click here
+          </button>
+          </Link>
         </section>
 
         <section id="main-img">
           <div className="black-line"></div>
           <img className="img" src={homepageImg} alt="gift-wrapped" />
-          <div>linha vermelha</div>
+          <img className="red-line image-position" src={redLineImg} />
         </section>
       </main>
     </>
