1d670239946c1e2c2096246e47f60ca605f164e4
Alissa Seixas <alissaps@gmail.com>
2021-11-21 22:21:30 -0300

    add random component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import GiftsList from './GiftsList';
+import RandomMessage from './RandomMessage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
       <Route path="/" element={<HomePage/>}  />
       <Route path="/giftslist" element={<GiftsList/>}  />
+      <Route path="/random" element={<RandomMessage/>}  />
 
         
       </Routes>
