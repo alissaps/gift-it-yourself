3469e9e7e9871ab5808f865914e8fe96583f036f
Alissa Seixas <alissaps@gmail.com>
2021-11-23 13:09:49 -0300

    add red lines and hover

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ import githubImg from "./assets/github-icon.png";
 import linkedinImg from "./assets/linkedin-icon.png";
 import emailImg from "./assets/email-icon.png";
 import "./Contact.css";
+import { ExternalLink } from "react-external-link";
 
 function Contact() {
   return (
@@ -11,27 +12,54 @@ function Contact() {
       <Navbar />
       <h1 className="mt-5">Contact Us</h1>
       <div className="contact-container">
-        <section className="contact-section">
-          <img alt="red line" className="red-line random-image-position" src={redLineImg} />
-          <div className="d-flex flex-column justify-content-center align-items-center">
-            <h2 className="first-contact">Alissa Seixas</h2>
-            <div className="d-flex">
-              <img alt="github" src={githubImg} />
-              <img alt="linkedin" src={linkedinImg} />
-              <img alt="email" src={emailImg} />
+        <section id="first-contact" className="contact-section">
+          <img
+            alt="red line"
+            className="first-contact-red-line"
+            src={redLineImg}
+          />
+          <div className="d-flex flex-column-reverse justify-content-center align-items-center">
+            
+            <div className="link-effect d-flex">
+              <ExternalLink href="https://github.com/alissaps">
+                <img alt="github" src={githubImg} />
+              </ExternalLink>
+
+              <ExternalLink href="https://www.linkedin.com/in/alissaseixas/">
+                <img alt="linkedin" src={linkedinImg} />
+              </ExternalLink>
+
+              <ExternalLink href="mailto:alissaps@gmail.com">
+                <img alt="email" src={emailImg} />
+              </ExternalLink>
             </div>
+            <h2 className="first-contact">Alissa Seixas</h2>
           </div>
         </section>
 
-        <section className="contact-section">
-          <img alt="red line" className="red-line random-image-position" src={redLineImg} />
-          <div className="d-flex flex-column justify-content-center align-items-center">
-            <h2 className="second-contact">Edgar Azevedo</h2>
-            <div className="d-flex">
-              <img alt="github" src={githubImg} />
-              <img alt="linkedin" src={linkedinImg} />
-              <img alt="email" src={emailImg} />
+        <section id="second-contact" className="contact-section">
+          <img
+            alt="red line"
+            className="second-contact-red-line"
+            src={redLineImg}
+          />
+
+          <div className="d-flex flex-column-reverse justify-content-center align-items-center">
+
+            <div className="link-effect d-flex">
+              <ExternalLink href="https://github.com/edgarvazevedo">
+                <img alt="github" src={githubImg} />{" "}
+              </ExternalLink>
+
+              <ExternalLink href="https://www.linkedin.com/in/edgarvazevedo/">
+                <img alt="linkedin" src={linkedinImg} />
+              </ExternalLink>
+
+              <ExternalLink href="mailto:edgarvazevedo@gmail.com">
+                <img alt="email" src={emailImg} />
+              </ExternalLink>
             </div>
+            <h2 className="second-contact">Edgar Azevedo</h2>
           </div>
         </section>
       </div>
